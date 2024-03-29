2a90a53cc7b6855d7e3c2ae2cc59ccabf758f7aa
AvgProgrammer <katsaros.lampros01@gmail.com>
2024-03-29 11:32:37 +0200

    Update the files xml files and Add the Filter.java

---
+++ src/main/java/org/example/Filter.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Filter implements Serializable {
+
+    private String area="";
+    private String time="";
+    private int numb=0;
+    private double price=0.0;
+    private double stars=0.0;
+
+    public Filter(String area,String time,int numb,double price,double stars){
+        this.area=area;
+        this.time=time;
+        this.numb=numb;
+        this.price=price;
+        this.stars=stars;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getNumb() {
+        return numb;
+    }
+
+    public void setNumb(int numb) {
+        this.numb = numb;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getStars() {
+        return stars;
+    }
+
+    public void setStars(double stars) {
+        this.stars = stars;
+    }
+}
