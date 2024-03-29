30cd7aed025bc106794a0837e1b7946e086d8905
AvgProgrammer <katsaros.lampros01@gmail.com>
2024-03-29 11:42:05 +0200

    Update the ManagerApp and Tenant to sent data to server

--- src/main/java/org/example/ManagerApp.java
+++ src/main/java/org/example/ManagerApp.java
@@ -1,24 +1,16 @@
 package org.example;
 
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
 import java.util.Scanner;
 
-public class ManagerApp {
+public class ManagerApp extends Thread{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Hey Please choose a role:\n1)Landlord\n2)Customer");
-        int role=sc.nextInt();
-        if(role==1) {
-            handleLandLord();
-        }else if(role==2){
-            handleTenant();
-        }else{
-            System.out.println("Wrong Input");
-        }
 
-    }
-
-    private static void handleLandLord(){
         int number;
         do {
             Scanner sc=new Scanner(System.in);
@@ -35,66 +27,39 @@ public class ManagerApp {
                     }*/
             }
         } while (number != 2);
+
     }
+    Socket requestSocket;
+    ObjectInputStream in=null;
+    ObjectOutputStream out=null;
+    private Room room;
 
-    private static void handleTenant(){
-        System.out.println("Hello Customer");
-        int answer=0;
-        String area="";
-        String time="";
-        int numb=0;
-        double price=0.0;
-        double stars=0.0;
-        Scanner sc=new Scanner(System.in);
-        int answer2=0;
-        do{
-            do {
-                System.out.println("Please Choose Filters:\n" +
-                        "1)Area\n" +
-                        "2)Time Period\n" +
-                        "3)Number of tenants\n" +
-                        "4)Price\n" +
-                        "5)Stars");
-                answer=sc.nextInt();
-                sc.nextLine();
-                switch (answer){
-                    case 1:
-                        System.out.println("Enter Area");
-                        area=sc.nextLine();
-                        break;
-                    case 2:
-                        System.out.println("Enter Time Period(DD-MM-YYYY-DD-MM-YYY)");
-                        time=sc.nextLine();
-                        break;
-                    case 3:
-                        do{
-                            System.out.println("Enter Number of tenants");
-                            numb=sc.nextInt();
-                            sc.nextLine();
-                        }while(numb<=0);
-                        break;
-                    case 4:
-                        do{
-                            System.out.println("Enter Price");
-                            price=sc.nextInt();
-                            sc.nextLine();
-                        }while (price<0);
-                        break;
-                    case 5:
-                        do{
-                            System.out.println("Enter Stars");
-                            stars=sc.nextDouble();
-                            sc.nextLine();
-                        }while (stars<0.0 || stars>5.0);
-                        break;
-                }
+    public ManagerApp(Room room){
+        this.room=room;
+    }
+    public void run() {
 
-            }while (answer<1 || answer>5);
-            System.out.println("Do you want to enter another filter:\n1)Yes\n2)No");
-            answer2= sc.nextInt();
-        }while (answer2==1);
-        Filter filter=new Filter(area,time,numb,price,stars);
-        sc.close();
+        try {
 
+            requestSocket=new Socket("127.0.0.1",1234);
+
+            this.out=new ObjectOutputStream(requestSocket.getOutputStream());
+            this.in=new ObjectInputStream(requestSocket.getInputStream());
+
+            this.out.writeObject(room);
+            this.out.flush();
+
+        } catch (UnknownHostException unknownHost) {
+            System.err.println("You are trying to connect to an unknown host!");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            try {
+                in.close();	out.close();
+                requestSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/org/example/Tenant.java
+++ src/main/java/org/example/Tenant.java
@@ -5,16 +5,76 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.rmi.UnknownHostException;
+import java.util.Scanner;
 
 public class Tenant extends Thread{
+
+    public static void main(String[] args){
+        System.out.println("Hello Customer");
+        int answer=0;
+        String area="";
+        String time="";
+        int numb=0;
+        double price=0.0;
+        double stars=0.0;
+        Scanner sc=new Scanner(System.in);
+        int answer2=0;
+        do{
+            do {
+                System.out.println("Please Choose Filters:\n" +
+                        "1)Area\n" +
+                        "2)Time Period\n" +
+                        "3)Number of tenants\n" +
+                        "4)Price\n" +
+                        "5)Stars");
+                answer=sc.nextInt();
+                sc.nextLine();
+                switch (answer){
+                    case 1:
+                        System.out.println("Enter Area");
+                        area=sc.nextLine();
+                        break;
+                    case 2:
+                        System.out.println("Enter Time Period(DD-MM-YYYY-DD-MM-YYY)");
+                        time=sc.nextLine();
+                        break;
+                    case 3:
+                        do{
+                            System.out.println("Enter Number of tenants");
+                            numb=sc.nextInt();
+                            sc.nextLine();
+                        }while(numb<=0);
+                        break;
+                    case 4:
+                        do{
+                            System.out.println("Enter Price");
+                            price=sc.nextInt();
+                            sc.nextLine();
+                        }while (price<0);
+                        break;
+                    case 5:
+                        do{
+                            System.out.println("Enter Stars");
+                            stars=sc.nextDouble();
+                            sc.nextLine();
+                        }while (stars<0.0 || stars>5.0);
+                        break;
+                }
+
+            }while (answer<1 || answer>5);
+            System.out.println("Do you want to enter another filter:\n1)Yes\n2)No");
+            answer2= sc.nextInt();
+        }while (answer2==1);
+        Filter filter=new Filter(area,time,numb,price,stars);
+        sc.close();
+    }
     Socket requestSocket;
     ObjectInputStream in=null;
     ObjectOutputStream out=null;
+    private Filter filter;
 
-    int a, b;
-    Tenant(int a, int b) {
-        this.a = a;
-        this.b = b;
+    Tenant(Filter filter) {
+        this.filter=filter;
     }
 
     public void run() {
@@ -28,12 +88,9 @@ public class Tenant extends Thread{
             this.in=new ObjectInputStream(requestSocket.getInputStream());
             /* Write the two integers */
 
-            this.out.writeInt(this.a);
+            this.out.writeObject(filter);
             this.out.flush();
-            this.out.writeInt(this.b);
-            this.out.flush();
-            /* Print the received result from server */
-            System.out.println("Server>" + in.readInt());
+
 
         } catch (UnknownHostException unknownHost) {
             System.err.println("You are trying to connect to an unknown host!");
